410f1413c14ba9a9a4d9827522141b2a2b78b74a
Jesse Lee Ross <jesseross001@gmail.com>
2023-10-23 15:17:06 +0000

    setting up things

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,42 @@ const API_URL ="https://ci-jshint.herokuapp.com/api";
 const resultsModal =new bootstrap.Modal(document.getElementById("resultsModal")); 
 
 document.getElementById("status").addEventListener("click",e => getStatus(e));
+document.getElementById("submit").addEventListener("click",e => postForm(e));
+
+async function postForm(e) {
+
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+
+}
+
+function displayErrors(data) {
+    let heading =`JSHINT results for ${data.file}`
+
+    if (data.total_errors === 0 ) {
+        results ='<div class="no_errors">No errors reported ! </div>';
+    }else {
+        results = '<div> Total Errors : <span class="error_count"> ${data.total_error} </span> </div>'
+    }
+
+    }
+}
+
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`
@@ -12,6 +48,15 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
-    }
+        console.log(data);
+    } else{ throw new Error(data.error);
+}} 
+
+function displayStatus(data) {
+document.getElementById("resultsModelTitle").innerText = heading;
+document.getElementById("results-content").innerText = results; 
+    resultsModal.show();
 }
+
+
+
